88fbb59d02fe763d8537c78dbcff4bf80678088e
Alexandros-Evald <alexandroslozi@gmail.com>
2024-09-27 10:32:54 +0300

    [Reviews] better approach on number check with modulus

--- src/components/directory-item/directory-item-component.js
+++ src/components/directory-item/directory-item-component.js
@@ -1,6 +1,8 @@
 import './directory-item.styles.scss';
 import {useNavigate} from "react-router-dom";
 import menImage from '../../assets/images/test.png';
+import reviewImage  from '../../assets/images/reviews.png';
+import toursImage  from '../../assets/images/Tours.png';
 
 const DirectoryItem = ({...props}) => {
 
@@ -9,7 +11,27 @@ const DirectoryItem = ({...props}) => {
 
 
     const onNavigateHandler = () => navigate(route);
-    const image = title === 'component #1' ? menImage : imageUrl;
+
+    const imageFacoty = (title) =>{
+        switch(title){
+            case 'component #1':
+                return menImage;
+                break;
+            case 'Reviews':
+                return reviewImage;
+                break;
+            case 'Tours':
+                return toursImage;
+                break;
+            default:
+                return imageUrl;
+                break;
+        }
+
+    }
+
+    const image = imageFacoty(title);
+
 
     return (
 
