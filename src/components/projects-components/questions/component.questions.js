f9d18ae46bc948b713efba01f4a92fe5daf6bd98
Alexandros-Evald <alexandroslozi@gmail.com>
2024-11-02 18:24:22 +0200

    implement a way to open only one question at a time

--- src/components/projects-components/questions/component.questions.js
+++ src/components/projects-components/questions/component.questions.js
@@ -1,9 +1,11 @@
 import SingleQuestion from "./component.singleQuestion";
 import "./questions.styles.scss";
+import {useState} from "react";
 
 
 const Questions = (props) => {
    const {questions} = props;
+   const [openedId, setOpenedId] = useState(null);
 
    return <div className='container'>
       <h3>Questions</h3>
@@ -11,7 +13,7 @@ const Questions = (props) => {
       <section className='info'>
          {questions.map((question) => {
             return (
-                <SingleQuestion key={question.id} {...question}></SingleQuestion>
+                <SingleQuestion key={question.id} openedId={openedId} setOpenedId={setOpenedId} {...question}></SingleQuestion>
             );
          })}
       </section>
--- src/components/projects-components/questions/component.singleQuestion.js
+++ src/components/projects-components/questions/component.singleQuestion.js
@@ -3,18 +3,23 @@ import {AiOutlineMinus, AiOutlinePlus} from "react-icons/ai";
 import {useState} from "react";
 import "./questions.styles.scss";
 
-const SingleQuestion = ({id,title,info}) => {
+const SingleQuestion = ({id, openedId, setOpenedId,title,info}) => {
 
     const [showInfo, setShowInfo] = useState(false);
+    const isActive = id === openedId;
+    const setToggle = () => {
+        setShowInfo(!showInfo);
+        setOpenedId(id);
+    }
 
     return <article className='question'>
         <header>
             <h4>{title}</h4>
-            <button className='btn' onClick={() => setShowInfo(!showInfo)}>
-                {showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+            <button className='btn' onClick={() => setToggle()}>
+                {showInfo && isActive ? <AiOutlineMinus/> : <AiOutlinePlus/>}
             </button>
         </header>
-        {showInfo && <p>{info}</p>}
+        {showInfo && isActive && <p>{info}</p>}
     </article>
 
 }
